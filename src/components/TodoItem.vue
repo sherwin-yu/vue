780232e5a5a099e83015e76259a34ecb53eabe17
Sherwin Yu <sherwinhyu@gmail.com>
2020-03-23 00:53:45 -0400

    add del button

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <div v-for="todo in todos" :key="todo">
-      <div class="todo-item">{{todo}}</div>
+      <div class="todo-item">
+        <div class="flex-container">
+          <div>{{todo}}</div>
+          <button>Delete</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -19,6 +24,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.flex-container {
+  display: flex;
+  justify-content: space-between;
+}
 .todo-item {
   padding: 10px 15px;
   background-color: #f6f6f6;
