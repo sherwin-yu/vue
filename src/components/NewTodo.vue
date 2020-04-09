96e573101647bfd71c9ef67919599cc28692e40f
Sherwin Yu <sherwinhyu@gmail.com>
2020-04-09 00:25:42 -0400

    add todo

--- src/components/NewTodo.vue
+++ src/components/NewTodo.vue
@@ -1,13 +1,39 @@
 <template>
-  <div>
-    <input class="text-input" type="text" />
-    <button class="submit" type="submit">Submit</button>
-  </div>
+  <form @submit="addTodo">
+    <input
+      class="text-input"
+      type="text"
+      v-model="name"
+      name="name"
+      placeholder="Add Todo..."
+      required
+    />
+    <button class="submit" type="submit" value="Submit">Submit</button>
+  </form>
 </template>
 
 <script>
+import { v4 as uuidv4 } from "uuid";
+
 export default {
-  name: "NewTodo"
+  name: "NewTodo",
+  data() {
+    return {
+      name: "",
+    };
+  },
+  methods: {
+    addTodo(e) {
+      e.preventDefault();
+      const newTodo = {
+        id: uuidv4(),
+        name: this.name,
+        isCompleted: false,
+      };
+      this.name = "";
+      this.$emit("add-todo", newTodo);
+    },
+  },
 };
 </script>
 
