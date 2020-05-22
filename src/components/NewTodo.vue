9e4da58149cc2adc7a5a4467e4385e440b39f2ff
Sherwin Yu <sherwinhyu@gmail.com>
2020-05-21 23:08:16 -0400

    make input a little prettier

--- src/components/NewTodo.vue
+++ src/components/NewTodo.vue
@@ -1,14 +1,16 @@
 <template>
   <form @submit="addTodo">
-    <input
-      class="text-input"
-      type="text"
-      v-model="name"
-      name="name"
-      placeholder="Add Todo..."
-      required
-    />
-    <button class="submit" type="submit" value="Submit">Submit</button>
+    <div class="flex-container">
+      <input
+        class="text-input"
+        type="text"
+        v-model="name"
+        name="name"
+        placeholder="Add Todo..."
+        required
+      />
+      <button class="submit" type="submit" value="Submit">Submit</button>
+    </div>
   </form>
 </template>
 
@@ -19,7 +21,7 @@ export default {
   name: "NewTodo",
   data() {
     return {
-      name: "",
+      name: ""
     };
   },
   methods: {
@@ -28,21 +30,26 @@ export default {
       const newTodo = {
         id: uuidv4(),
         name: this.name,
-        isCompleted: false,
+        isCompleted: false
       };
       this.name = "";
       this.$emit("add-todo", newTodo);
-    },
-  },
+    }
+  }
 };
 </script>
 
 <style scoped>
+.flex-container {
+  display: flex;
+}
+
 .text-input {
   padding: 10px 15px;
+  width: 100%;
 }
 .submit {
-  padding: 10px 15px;
+  padding: 10px 25px;
   background-color: #42b983;
   border: 1px solid transparent;
   color: white;
